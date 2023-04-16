4b6aea894287d75383c955f7adceffcb41ce5a3a
Alexandre Chambu <amanichambu48@gmail.com>
2023-04-16 15:32:37 +0200

    Delete components/ui/dist directory

--- components/ui/dist/Button.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Button } from "antd";
-const BtnSubmit = ({ children }) => {
-  return /* @__PURE__ */ React.createElement(Button, null, children);
-};
-export default BtnSubmit;
